14a555f157be6e157f759ee9062e3bffff7dfa24
spar234 <87600450+spar234@users.noreply.github.com>
2021-07-24 12:12:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+canvas =
+document.getElementById("myCanvas");
+ctx= canvas.getElementById("2d");
+ctx.beginPath();
+ctx.strokenStyel = "red";
+ctx.lineWidth = 1;
+ctx.rect(150, 143, 430, 200);
+ctx.stroken();
